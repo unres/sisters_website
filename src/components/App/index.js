65d90639ece57f8a1c0583dc7e05e5352f258ab0
unres <timothy.bell@ttu.edu>
2021-01-21 18:07:21 -0600

    Edited  file structure and got working background

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import Temp from '../Temp'
-import './App.css'
+import Temp from "../Temp";
 
+import "./App.css";
 
+//https://itnext.io/responsive-background-images-using-react-hooks-941af365ea1f
 
 const App = () => {
   return (
-    <div className="container">
-      <Temp />
+    <div className="App" style={{ backgroundImage: "url(/img/IMG_8858.jpg)" }}>
+      <div className="App-content">
+        <h1>Coming Soon!</h1>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
